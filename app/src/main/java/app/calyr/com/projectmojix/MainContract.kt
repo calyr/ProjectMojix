2f8b998f36d6f385f189445f3d71c216f2b26019
Roberto Carlos Callisaya Mamani <calyr_software@hotmail.com>
2018-11-23 18:10:12 -0400

    Init project

---
+++ app/src/main/java/app/calyr/com/projectmojix/MainContract.kt
@@ -0,0 +1,6 @@
+package app.calyr.com.projectmojix
+
+import java.util.*
+
+
+data class User( val name: String? = null, val address: String? = null, val birthDate: Date, val phoneNumber: String? = null, val email: String? = null )
